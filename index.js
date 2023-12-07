0bb666765d19a2d4653aa233e7291654699f476e
ChefTuber <153126003+ChefTuber@users.noreply.github.com>
2023-12-07 13:42:25 +0000

    index.**x

---
+++ index.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#proxyButton').click(function() {
+        // Perform proxy request to access Roblox
+        alert('Accessing Roblox through proxy...');
+    });
+});
